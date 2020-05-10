40cc341b7fe4cded0901764a495e707964769da0
nicoale <nicoale8786@gmail.com>
2020-05-10 13:52:02 +0300

    enabled the react app to communicate with the rails app

---
+++ ReactInsure/src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutPage  = () => {
+    return (
+      <div>
+           <div>
+                <h1>Insure About Page</h1>
+                <h1>Insure About Page</h1>
+            </div>
+      </div>
+       
+    );
+  }
+
+  export default AboutPage;
