5891093c22032b1c0216c37c05523e9146073b81
nicoale <nicoale8786@gmail.com>
2020-05-10 12:42:30 +0300

    altered the file to ensure cookies are stored well  no matter the environment

--- config/initializers/sessions_store.rb
+++ config/initializers/sessions_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_insure", domain: "insure-app.herokuapp.com"
+if Rails.env =="production"
+    Rails.application.config.session_store :cookie_store, key: "_insure", domain: "insure-app.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_insure"
+end
